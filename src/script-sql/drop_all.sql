0016a7cdc2294b4e6d6d69abdb0214648686222f
Giovanni Liboni <giovanni.liboni@live.com>
2014-06-18 17:37:47 +0200

    Aggiornato drop.sql file

--- src/script-sql/drop_all.sql
+++ src/script-sql/drop_all.sql
@@ -1,7 +1,9 @@
-drop table posto cascade;
+﻿drop table posto cascade;
 drop table imbarco cascade;
 drop table passeggero cascade;
 drop table prenotazione cascade;
 drop table volo cascade;
 drop table biglietto cascade;
 drop table tratta cascade;
+drop function update_time_richiesta;
+drop function update_time_biglietto;
